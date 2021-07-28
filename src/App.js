2cb01f3cc0a500f14d15befdc35bbf7f38843f2f
Rishi Choubey <rishichoubey436@gmail.com>
2021-07-28 16:49:46 +0530

    my first commit react covid proj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch("https://api.covid19india.org/state_district_wise.json").then(
+      (result) => {
+        result.json().then((resp) => {
+          setData(resp);
+        })
+      })
+  }, []);
+  console.warn(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GET API CALL </h1>
+      <table>
+        <thead>
+
+        </thead>
+        <tbody>
+        <tr>
+          <td>notes</td>
+          <td>active</td>
+          <td>confirmed</td>
+          <td>migratedother</td>
+          <td>deceased</td>
+          <td>recovered</td>
+          <td>confirmed</td>
+          <td>deceased</td>
+          <td>recovered</td>
+        </tr>
+        {
+          [data].map((covid) => 
+          <tr>
+            <td>{covid.notes}</td>
+            <td>{covid.active}</td>
+            <td>{covid.confirmed}</td>
+            <td>{covid.migratedother}</td>
+            <td>{covid.deceased}</td>
+            <td>{covid.recovered}</td>
+            <td>{covid.confirmed}</td>
+            <td>{covid.deceased}</td>
+            <td>{covid.recovered}</td>
+          </tr>
+          )
+        }
+        </tbody>
+      </table>
     </div>
   );
 }
